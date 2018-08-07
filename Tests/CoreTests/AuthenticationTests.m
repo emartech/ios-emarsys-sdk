1fdd6464f7c8c30a0f29608bd827fe99ac9c8bfb
stoto <pstojcsics@emarsys.com>
2018-08-07 12:31:39 +0200

    test(nullability-warnings): fix some nullability warnings in tests SUITEDEV-13826

--- Tests/CoreTests/AuthenticationTests.m
+++ Tests/CoreTests/AuthenticationTests.m
@@ -7,47 +7,49 @@
 
 SPEC_BEGIN(AuthenticationTests)
 
-    __block NSTimeZone *cachedTimeZone;
+        __block NSTimeZone *cachedTimeZone;
 
-    beforeAll(^{
-        cachedTimeZone = [NSTimeZone defaultTimeZone];
-        [NSTimeZone setDefaultTimeZone:[NSTimeZone timeZoneWithName:@"Europe/Budapest"]];
-    });
-
-    afterAll(^{
-        [NSTimeZone setDefaultTimeZone:cachedTimeZone];
-    });
-
-    describe(@"NSString+CoreTests createBasicAuthWith:(NSString *)username password:(NSString *)password", ^{
-        it(@"should throw exception when username is nil", ^{
-            @try {
-                [EMSAuthentication createBasicAuthWithUsername:nil
-                                                      password:@"pass"];
-                fail(@"Expected exception when username is nil");
-            } @catch (NSException *exception) {
-                [[theValue(exception) shouldNot] beNil];
-            }
+        beforeAll(^{
+            cachedTimeZone = [NSTimeZone defaultTimeZone];
+            [NSTimeZone setDefaultTimeZone:[NSTimeZone timeZoneWithName:@"Europe/Budapest"]];
         });
 
-        it(@"should throw exception when password is nil", ^{
-            @try {
-                [EMSAuthentication createBasicAuthWithUsername:@"valami"
-                                                      password:nil];
-                fail(@"Expected exception when password is nil");
-            } @catch (NSException *exception) {
-                [[theValue(exception) shouldNot] beNil];
-            }
+        afterAll(^{
+            [NSTimeZone setDefaultTimeZone:cachedTimeZone];
         });
 
-        it(@"should create the correct basicAuth when username and password set", ^{
-            NSString *basicAuth1 = [EMSAuthentication createBasicAuthWithUsername:@"user"
-                                                                         password:@"pass"];
-            [[basicAuth1 should] equal:@"Basic dXNlcjpwYXNz"];
+        describe(@"NSString+CoreTests createBasicAuthWith:(NSString *)username password:(NSString *)password", ^{
+            it(@"should throw exception when username is nil", ^{
+                @try {
+                    IGNORE_NONNULL_BEGIN
+                    [EMSAuthentication createBasicAuthWithUsername:nil
+                                                          password:@"pass"];
+                    IGNORE_NONNULL_END
+                    fail(@"Expected exception when username is nil");
+                } @catch (NSException *exception) {
+                    [[theValue(exception) shouldNot] beNil];
+                }
+            });
 
-            NSString *basicAuth2 = [EMSAuthentication createBasicAuthWithUsername:@"user2"
-                                                                         password:@"pass2"];
-            [[basicAuth2 should] equal:@"Basic dXNlcjI6cGFzczI="];
+            it(@"should throw exception when password is nil", ^{
+                @try {
+                    [EMSAuthentication createBasicAuthWithUsername:@"valami"
+                                                          password:nil];
+                    fail(@"Expected exception when password is nil");
+                } @catch (NSException *exception) {
+                    [[theValue(exception) shouldNot] beNil];
+                }
+            });
+
+            it(@"should create the correct basicAuth when username and password set", ^{
+                NSString *basicAuth1 = [EMSAuthentication createBasicAuthWithUsername:@"user"
+                                                                             password:@"pass"];
+                [[basicAuth1 should] equal:@"Basic dXNlcjpwYXNz"];
+
+                NSString *basicAuth2 = [EMSAuthentication createBasicAuthWithUsername:@"user2"
+                                                                             password:@"pass2"];
+                [[basicAuth2 should] equal:@"Basic dXNlcjI6cGFzczI="];
+            });
         });
-    });
 
 SPEC_END
