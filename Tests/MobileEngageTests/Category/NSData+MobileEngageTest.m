9e5911aaf5d4a6908aaed76b7df6b31f33635e4e
dschuppa <d.schuppa@gmail.com>
2019-09-24 09:07:18 +0200

    test(devicetoken): modified for iOS13 SUITEDEV-19173
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- Tests/MobileEngageTests/Category/NSData+MobileEngageTest.m
+++ Tests/MobileEngageTests/Category/NSData+MobileEngageTest.m
@@ -7,11 +7,13 @@ SPEC_BEGIN(NSDataMobileEngageTests)
 
         it(@"should transform data into string", ^{
             char *rawData = "1234512345ABCDEF";
-            NSData *data = [NSData dataWithBytes:rawData length:sizeof(rawData)];
-            NSString *token = [[[data description]
-                    stringByTrimmingCharactersInSet:[NSCharacterSet characterSetWithCharactersInString:@"<>"]]
-                    stringByReplacingOccurrencesOfString:@" " withString:@""];
-            [[[data deviceTokenString] should] equal:token];
+            NSData *deviceToken = [NSData dataWithBytes:rawData length:sizeof(rawData)];
+            NSMutableString *token = [NSMutableString new];
+            const char *data = [deviceToken bytes];
+            for (int i = 0; i < deviceToken.length; ++i) {
+                [token appendFormat:@"%02.2hhx", data[i]];
+            }
+            [[[deviceToken deviceTokenString] should] equal:token];
         });
 
     });
