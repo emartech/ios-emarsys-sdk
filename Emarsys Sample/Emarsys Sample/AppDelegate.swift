a608c9bdbfb327a15463a4a9644d0a6c6608d711
dschuppa <d.schuppa@gmail.com>
2019-11-14 13:11:22 +0100

    feat(sample-app) use EMSAppDelegate in sample app SUITEDEV-19744
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>

--- Emarsys Sample/Emarsys Sample/AppDelegate.swift
+++ Emarsys Sample/Emarsys Sample/AppDelegate.swift
@@ -2,69 +2,30 @@
 //  Copyright (c) 2017 Emarsys. All rights reserved.
 //
 
-import UIKit
-import UserNotifications
 import EmarsysSDK
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, EMSEventHandler {
+class AppDelegate: EMSAppDelegate {
 
-    var window: UIWindow?
-
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-
-        window?.tintColor = UIColor(red: 101 / 255.0, green: 151 / 255.0, blue: 207 / 255.0, alpha: 1.0)
-
-
-        let config = EMSConfig.make { builder in
+    override func provideEMSConfig() -> EMSConfig! {
+        EMSConfig.make { builder in
             builder.setMerchantId("1428C8EE286EC34B")
             builder.setContactFieldId(3)
-#if DEBUG
-            builder.setMobileEngageApplicationCode("EMS11-C3FD3")
-#else
-            builder.setMobileEngageApplicationCode("EMS4C-9A869")
-#endif
+            #if DEBUG
+                builder.setMobileEngageApplicationCode("EMS11-C3FD3")
+            #else
+                builder.setMobileEngageApplicationCode("EMS4C-9A869")
+            #endif
         }
-        Emarsys.setup(with: config)
-        Emarsys.inApp.eventHandler = self
-        Emarsys.notificationCenterDelegate.eventHandler = self
-        UNUserNotificationCenter.current().delegate = Emarsys.notificationCenterDelegate
-
-        application.registerForRemoteNotifications()
-
-        var options: UNAuthorizationOptions = [.alert, .sound, .badge]
-        if #available(iOS 12.0, *) {
-            options.insert(.provisional)
-        }
-        UNUserNotificationCenter.current().requestAuthorization(options: options) { [unowned self] granted, error in
-            print(granted, error ?? "no error")
-            if (granted) {
-            }
-        }
-
-        return true
     }
 
-    func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([Any]?) -> Void) -> Bool {
-        return Emarsys.trackDeepLink(with: userActivity, sourceHandler: { url in
-            print(url)
-        })
+    override func handleEvent(_ eventName: String, payload: [String: NSObject]?) {
+        super.handleEvent(eventName, payload: payload)
+        print("EVENT_NAME: \(eventName), PAYLOAD: \(payload ?? [:])")
     }
 
-    func handleEvent(_ eventName: String, payload: [String: NSObject]?) {
-        print(eventName, payload);
-    }
-
-    func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
-        Emarsys.push.setPushToken(deviceToken)
+    override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
+        super.application(application, didRegisterForRemoteNotificationsWithDeviceToken: deviceToken)
         NotificationCenter.default.post(name: NotificationNames.pushTokenArrived.asNotificationName(), object: nil, userInfo: ["push_token": deviceToken])
     }
-
-    func application(_ application: UIApplication, didFailToRegisterForRemoteNotificationsWithError error: Error) {
-    }
-
-    func application(_ application: UIApplication, didReceiveRemoteNotification userInfo: [AnyHashable: Any], fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
-        Emarsys.push.trackMessageOpen(userInfo: userInfo)
-    }
-
 }
