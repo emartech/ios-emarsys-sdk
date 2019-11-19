4a0247960c4775bbfd30522f495201451b9c130d
Mikael Lechat <mikael.lechat@poqcommerce.com>
2019-11-19 15:17:08 +0000

    Sort header files alphabetically

--- EmarsysSDK/EmarsysSDK.h
+++ EmarsysSDK/EmarsysSDK.h
@@ -3,26 +3,26 @@
 FOUNDATION_EXPORT double CoreSDKVersionNumber;
 FOUNDATION_EXPORT const unsigned char CoreSDKVersionString[];
 
-#import <Core/EMSDeviceInfo.h>
-#import <Core/Flipper/EMSFlipperFeatures.h>
 #import <EmarsysSDK/Emarsys.h>
 #import <EmarsysSDK/EMSAppDelegate.h>
 #import <EmarsysSDK/EMSBlocks.h>
+#import <EmarsysSDK/EMSCartItem.h>
+#import <EmarsysSDK/EMSCartItemProtocol.h>
 #import <EmarsysSDK/EMSConfig.h>
 #import <EmarsysSDK/EMSConfigBuilder.h>
 #import <EmarsysSDK/EMSConfigProtocol.h>
+#import <EmarsysSDK/EMSDeviceInfo.h>
+#import <EmarsysSDK/EMSEventHandler.h>
+#import <EmarsysSDK/EMSFlipperFeatures.h>
 #import <EmarsysSDK/EMSInAppProtocol.h>
 #import <EmarsysSDK/EMSInboxProtocol.h>
+#import <EmarsysSDK/EMSLogic.h>
+#import <EmarsysSDK/EMSLogicProtocol.h>
+#import <EmarsysSDK/EMSNotification.h>
+#import <EmarsysSDK/EMSNotificationInboxStatus.h>
 #import <EmarsysSDK/EMSPredictProtocol.h>
+#import <EmarsysSDK/EMSProduct.h>
 #import <EmarsysSDK/EMSPushNotificationProtocol.h>
-#import <MobileEngage/EMSEventHandler.h>
-#import <MobileEngage/EMSNotification.h>
-#import <MobileEngage/EMSNotificationInboxStatus.h>
-#import <MobileEngage/EMSUserNotificationCenterDelegate.h>
-#import <Predict/EMSCartItem.h>
-#import <Predict/EMSCartItemProtocol.h>
-#import <Predict/EMSLogic.h>
-#import <Predict/EMSLogicProtocol.h>
-#import <Predict/EMSProduct.h>
-#import <Predict/EMSRecommendationFilter.h>
-#import <Predict/EMSRecommendationFilterProtocol.h>
+#import <EmarsysSDK/EMSRecommendationFilter.h>
+#import <EmarsysSDK/EMSRecommendationFilterProtocol.h>
+#import <EmarsysSDK/EMSUserNotificationCenterDelegate.h>
