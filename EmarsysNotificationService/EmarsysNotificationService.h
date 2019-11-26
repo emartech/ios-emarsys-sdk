97af977d4f77c812e24e160fee1dd82ef984d43f
Mikael Lechat <mikael.lechat@poqcommerce.com>
2019-11-26 14:31:02 +0000

    Add target to create EmarsysNotificationService.framework for rich push notifications

---
+++ EmarsysNotificationService/EmarsysNotificationService.h
@@ -0,0 +1,6 @@
+#import <UIKit/UIKit.h>
+
+FOUNDATION_EXPORT double EmarsysNotificationServiceVersionNumber;
+FOUNDATION_EXPORT const unsigned char EmarsysNotificationServiceVersionString[];
+
+#import <EmarsysNotificationService/EMSNotificationService.h>
