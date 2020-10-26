0506c45314187e45ef8a82bad57a5aee12760e15
LasOri <laszlo.oeri@gmail.com>
2020-10-26 16:02:33 +0100

    test(e2e): e2e test for changeApplicationCode SUITEDEV-25088
    Co-authored-by: kovacszsoltizsolt <22084766+kovacszsoltizsolt@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

---
+++ Tests/EmarsysSDKTests/EmarsysE2ETests.swift
@@ -0,0 +1,113 @@
+//
+//  Copyright © 2020. Emarsys. All rights reserved.
+//
+
+import XCTest
+
+class EmarsysE2ETests: XCTestCase {
+    
+    let timeout = 2.0
+
+    override func setUpWithError() throws {
+        
+    }
+
+    override func tearDownWithError() throws {
+        EmarsysTestUtils.tearDownEmarsys()
+    }
+
+    func testChangeApplicationCodeFromNil() throws {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let timestamp = dateFormatter.string(from: Date())
+        let config = EMSConfig.make { builder in
+        }
+
+        Emarsys.setup(with: config)
+        
+        changeAppCode("EMS11-C3FD3", cId: 3)
+        
+        setContact("test@test.com")
+        
+        sendEvent("iosE2EChangeAppCodeFromNil", timestamp: timestamp)
+        
+        waitForBE()
+        
+        _ = filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+    }
+    
+    func testChangeApplicationCode() throws {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        
+        let timestamp = dateFormatter.string(from: Date())
+        let config = EMSConfig.make { builder in
+            builder.setMobileEngageApplicationCode("14C19-A121F")
+            builder.setContactFieldId(3)
+        }
+
+        Emarsys.setup(with: config)
+        
+        changeAppCode("EMS11-C3FD3", cId: 3)
+        
+        setContact("test@test.com")
+        
+        sendEvent("iosE2EChangeAppCodeFromNil", timestamp: timestamp)
+        
+        waitForBE()
+        
+        _ = filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+    }
+    
+    func changeAppCode(_ code: String, cId: NSNumber) {
+        let changeAppCodeExpectation = XCTestExpectation(description: "waitForResult")
+        Emarsys.config.changeApplicationCode(code, contactFieldId: cId) { error in
+            XCTAssertNil(error)
+            changeAppCodeExpectation.fulfill()
+        }
+        _ = XCTWaiter.wait(for: [changeAppCodeExpectation], timeout: timeout)
+    }
+    
+    func setContact(_ cValue: String) {
+        let contactExpectation = XCTestExpectation(description: "waitForResult")
+        Emarsys.setContactWithContactFieldValue(cValue) { error in
+            XCTAssertNil(error)
+            contactExpectation.fulfill()
+        }
+        _ = XCTWaiter.wait(for: [contactExpectation], timeout: timeout)
+
+    }
+    
+    func sendEvent(_ name: String, timestamp: String) {
+        let customEventExpectation = XCTestExpectation(description: "waitForResult")
+        Emarsys.trackCustomEvent(withName: "emarsys-sdk-e2e-test", eventAttributes: [
+            "eventName": name,
+            "timestamp": timestamp
+        ]) { error in
+            customEventExpectation.fulfill()
+        }
+        _ = XCTWaiter.wait(for: [customEventExpectation], timeout: 2)
+    }
+    
+    func  waitForBE() {
+        usleep(2000000)
+    }
+    
+    func filterForInboxMessage(_ title: String, body: String) -> EMSMessage {
+        var inboxMessage: EMSMessage?
+        let fetchMessagesExpectation = XCTestExpectation(description: "waitForResult")
+        Emarsys.messageInbox.fetchMessages { (inboxResult, error) in
+            inboxMessage = inboxResult?.messages.first(where: { (message) -> Bool in
+                return message.title == title && message.body == body
+            })
+            fetchMessagesExpectation.fulfill()
+        }
+        _ = XCTWaiter.wait(for: [fetchMessagesExpectation], timeout: 2)
+        
+        XCTAssertNotNil(inboxMessage)
+        
+        return inboxMessage!
+    }
+    
+}
--- Tests/EmarsysSDKTests/EmarsysSDKTests-Bridging-Header.h
+++ Tests/EmarsysSDKTests/EmarsysSDKTests-Bridging-Header.h
@@ -2,9 +2,5 @@
 //  Use this file to import your target's public headers that you would like to expose to Swift.
 //
 
-#import "EMSProductMapper.h"
-#import "EMSResponseModel.h"
-#import "EMSProduct.h"
-#import "EMSRequestModel.h"
 #import "Emarsys.h"
 #import "EmarsysTestUtils.h"
