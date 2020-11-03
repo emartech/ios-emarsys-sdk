15f566c76aef3fdf35f282164de7e49f72e12c99
LasOri <laszlo.oeri@gmail.com>
2020-11-03 11:45:40 +0100

    test(e2e): fix inbox tests SUITEDEV-25201
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- Tests/EmarsysSDKTests/EmarsysE2ETests.swift
+++ Tests/EmarsysSDKTests/EmarsysE2ETests.swift
@@ -31,16 +31,17 @@ class EmarsysE2ETests: XCTestCase {
         setContact("test@test.com")
         
         sendEvent("iosE2EChangeAppCodeFromNil", timestamp: timestamp)
-        
-        waitForBE()
-        
-        _ = filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+
+        retry { [unowned self] () in
+
+            _ = try filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+        }
     }
-    
+
     func testChangeApplicationCode() throws {
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-        
+
         let timestamp = dateFormatter.string(from: Date())
         let config = EMSConfig.make { builder in
             builder.setMobileEngageApplicationCode("14C19-A121F")
@@ -48,19 +49,25 @@ class EmarsysE2ETests: XCTestCase {
         }
 
         Emarsys.setup(with: config)
-        
+
         changeAppCode("EMS11-C3FD3", cId: 2575)
-        
+
         setContact("test@test.com")
-        
+
         sendEvent("iosE2EChangeAppCodeFromNil", timestamp: timestamp)
-        
-        waitForBE()
-        
-        _ = filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+
+        retry { [unowned self] () in
+
+            _ = try filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+        }
     }
 
     func testInboxTags() {
+        enum InboxError: Error {
+            case missingTag
+            case existingTag
+        }
+
         let dateFormatter = DateFormatter()
         dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
 
@@ -76,25 +83,40 @@ class EmarsysE2ETests: XCTestCase {
 
         sendEvent("iosE2EChangeAppCodeFromNil", timestamp: timestamp)
 
-        waitForBE()
+        retry { [unowned self] () in
 
-        let message = filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+            let message = try filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
 
-        Emarsys.messageInbox.addTag("testtag", forMessage: message.id)
+            Emarsys.messageInbox.addTag("testtag", forMessage: message.id)
+        }
 
-        waitForBE()
+        var messageWithTag: EMSMessage?
 
-        let messageWithTag = filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+        retry { [unowned self] () in
 
-        XCTAssertTrue(messageWithTag.tags!.contains("testtag"))
+            messageWithTag = try filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
 
-        Emarsys.messageInbox.removeTag("testtag", fromMessage: messageWithTag.id)
+            if let tags = messageWithTag?.tags, !tags.contains("testtag") {
+                throw InboxError.missingTag
+            }
+        }
 
-        waitForBE()
+        XCTAssertTrue(messageWithTag?.tags?.contains("testtag") ?? false)
 
-        let messageWithoutTag = filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+        Emarsys.messageInbox.removeTag("testtag", fromMessage: messageWithTag?.id ?? "")
 
-        XCTAssertFalse(messageWithoutTag.tags!.contains("testtag"))
+        var messageWithoutTag: EMSMessage?
+
+        retry { [unowned self] () in
+
+            messageWithoutTag = try filterForInboxMessage("iosE2EChangeAppCodeFromNil", body: timestamp)
+
+            if let tags = messageWithoutTag?.tags, tags.contains("testtag") {
+                throw InboxError.existingTag
+            }
+        }
+
+        XCTAssertFalse(messageWithoutTag?.tags?.contains("testtag") ?? true)
     }
     
     func changeAppCode(_ code: String, cId: NSNumber) {
@@ -118,7 +140,7 @@ class EmarsysE2ETests: XCTestCase {
     
     func sendEvent(_ name: String, timestamp: String) {
         let customEventExpectation = XCTestExpectation(description: "waitForResult")
-        Emarsys.trackCustomEvent(withName: "emarsys-sdk-e2e-test", eventAttributes: [
+        Emarsys.trackCustomEvent(withName: "emarsys-sdk-e2e-inbox-test", eventAttributes: [
             "eventName": name,
             "timestamp": timestamp
         ]) { error in
@@ -127,11 +149,11 @@ class EmarsysE2ETests: XCTestCase {
         _ = XCTWaiter.wait(for: [customEventExpectation], timeout: 2)
     }
     
-    func  waitForBE() {
-        usleep(3000000)
-    }
-    
-    func filterForInboxMessage(_ title: String, body: String) -> EMSMessage {
+    func filterForInboxMessage(_ title: String, body: String) throws -> EMSMessage {
+        enum InboxError: Error {
+            case missingMessage
+        }
+
         var inboxMessage: EMSMessage?
         let fetchMessagesExpectation = XCTestExpectation(description: "waitForResult")
         Emarsys.messageInbox.fetchMessages { (inboxResult, error) in
@@ -141,10 +163,35 @@ class EmarsysE2ETests: XCTestCase {
             fetchMessagesExpectation.fulfill()
         }
         _ = XCTWaiter.wait(for: [fetchMessagesExpectation], timeout: 2)
+
+        guard let message = inboxMessage else {
+            throw InboxError.missingMessage
+        }
         
-        XCTAssertNotNil(inboxMessage)
-        
-        return inboxMessage!
+        return message
     }
     
+}
+
+extension XCTestCase {
+
+    func retry(retryCount: Int = 3, delay: Double? = 1.0, retryClosure: @escaping () throws -> ()) {
+        var error: Error?
+        var index = 0
+        repeat {
+            error = nil
+            do {
+                try retryClosure()
+                index += 1
+            } catch let e {
+                error = e
+            }
+            if error != nil, index < retryCount, let delay = delay {
+                Thread.sleep(forTimeInterval: delay)
+            }
+        } while (error != nil && index < retryCount)
+
+        XCTAssertNil(error)
+    }
+
 }
