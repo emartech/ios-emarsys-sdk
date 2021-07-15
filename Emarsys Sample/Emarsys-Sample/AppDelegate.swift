9ba2d6fb053e23965804b0d6216026c4a7e810f0
LasOri <laszlo.oeri@gmail.com>
2021-07-15 14:12:41 +0200

    feat(event-handler): modify eventHandler usage SUITEDEV-28441
    Co-authored-by: LordAndras <49073629+LordAndras@users.noreply.github.com>

--- Emarsys Sample/Emarsys-Sample/AppDelegate.swift
+++ Emarsys Sample/Emarsys-Sample/AppDelegate.swift
@@ -10,6 +10,22 @@ import EmarsysSDK
 class AppDelegate: EMSAppDelegate {
     
     override func provideEMSConfig() -> EMSConfig! {
+        self.eventHandler = { eventName, payload in
+            let alertController = UIAlertController(title: eventName, message: "\(String(describing: payload))", preferredStyle: .alert)
+            alertController.addAction(UIAlertAction(title: "Close", style: .destructive, handler: { (action) in
+                alertController.dismiss(animated: true, completion: nil)
+            }))
+            
+            UIApplication.shared.windows.first?.rootViewController?.present(alertController, animated: true, completion: nil)
+            print("EVENT_NAME: \(eventName), PAYLOAD: \(payload ?? [:])")
+            
+            let content = UNMutableNotificationContent()
+            content.title = eventName
+            content.body = payload!.description
+            let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
+            let request = UNNotificationRequest(identifier: "notification.id.01", content: content, trigger: trigger)
+            UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
+        }
         let userDefaults = UserDefaults.init(suiteName: "com.emarsys.sampleConfig")
         return EMSConfig.make { builder in
             if let appCode = userDefaults?.string(forKey: ConfigUserDefaultsKey.applicationCode.rawValue) {
@@ -37,24 +53,6 @@ class AppDelegate: EMSAppDelegate {
             print("device did shake")
         }
         return true
-    }
-
-    override func handleEvent(_ eventName: String, payload: [String: NSObject]?) {
-        super.handleEvent(eventName, payload: payload)
-        let alertController = UIAlertController(title: eventName, message: "\(String(describing: payload))", preferredStyle: .alert)
-        alertController.addAction(UIAlertAction(title: "Close", style: .destructive, handler: { (action) in
-            alertController.dismiss(animated: true, completion: nil)
-        }))
-        
-        UIApplication.shared.windows.first?.rootViewController?.present(alertController, animated: true, completion: nil)
-        print("EVENT_NAME: \(eventName), PAYLOAD: \(payload ?? [:])")
-        
-        let content = UNMutableNotificationContent()
-        content.title = eventName
-        content.body = payload!.description
-        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1, repeats: false)
-        let request = UNNotificationRequest(identifier: "notification.id.01", content: content, trigger: trigger)
-        UNUserNotificationCenter.current().add(request, withCompletionHandler: nil)
     }
     
     override func application(_ application: UIApplication, didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data) {
