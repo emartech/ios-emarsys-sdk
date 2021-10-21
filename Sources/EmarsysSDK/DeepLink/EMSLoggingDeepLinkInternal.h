12bb8a8ee719ab02689e0639e12bbf248bb5a28b
LasOri <laszlo.oeri@gmail.com>
2021-10-21 14:45:45 +0200

    fix(logging): fix import of logging deeplink internal SUITEDEV-29147
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- Sources/EmarsysSDK/DeepLink/EMSLoggingDeepLinkInternal.h
+++ Sources/EmarsysSDK/DeepLink/EMSLoggingDeepLinkInternal.h
@@ -2,7 +2,7 @@
 // Copyright (c) 2019 Emarsys. All rights reserved.
 //
 #import <Foundation/Foundation.h>
-#import "../EMSDeepLinkProtocol.h"
+#import "EMSDeepLinkProtocol.h"
 
 @interface EMSLoggingDeepLinkInternal : NSObject <EMSDeepLinkProtocol>
 @end
