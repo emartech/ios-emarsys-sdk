495d914a9696f396c089220d1ab36aeb7d2585a9
davidSchuppa <d.schuppa@gmail.com>
2022-12-08 16:40:26 +0100

    chore(project): added mocking framework dependency SUITEDEV-32504
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

---
+++ EmarsysSDKTests/EmarsysSDKTests.swift
@@ -0,0 +1,8 @@
+import XCTest
+@testable import EmarsysSDK
+
+final class EmarsysSDKTests: XCTestCase {
+    func testExample() throws {
+
+    }
+}
--- Package.swift
+++ Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "EmarsysSDK",
+    platforms: [.iOS(.v13)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
@@ -13,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .package(url: "https://github.com/MakeAWishFoundation/SwiftyMocky", from: "4.2.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
@@ -23,6 +24,6 @@ let package = Package(
             dependencies: []),
         .testTarget(
             name: "EmarsysSDKTests",
-            dependencies: ["EmarsysSDK"]),
+            dependencies: ["EmarsysSDK", "SwiftyMocky"]),
     ]
 )
