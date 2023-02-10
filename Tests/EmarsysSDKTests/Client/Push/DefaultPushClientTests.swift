5375cdb73c915723a0968588de133653dd3e6347
davidSchuppa <d.schuppa@gmail.com>
2023-02-10 13:06:27 +0100

    test(push-client): add missing test cases to DefaultPushClient SUITEDEV-32931
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- Tests/EmarsysSDKTests/Client/Push/DefaultPushClientTests.swift
+++ Tests/EmarsysSDKTests/Client/Push/DefaultPushClientTests.swift
@@ -6,7 +6,7 @@ import XCTest
 
 @SdkActor
 final class DefaultPushClientTests: XCTestCase {
-
+    
     @Inject(\.genericNetworkClient)
     var fakeNetworkClient: FakeGenericNetworkClient
     
@@ -25,6 +25,50 @@ final class DefaultPushClientTests: XCTestCase {
                                        sdkContext: sdkContext,
                                        sdkLogger: sdkLogger)
     }
+    
+    
+    func testRegisterPushToken_shouldRegisterTokenWithEmarsysClient() async throws {
+        let pushToken = "testPushToken"
+        let expectedBody = ["pushToken": pushToken].toData()
 
-// TODO: tests
+        var count = 0
+        var requestUrl: String? = nil
+        var sentBody: Data? = nil
+        
+        fakeNetworkClient.when(\.send) { invocationCount, params in
+            count = invocationCount
+            let request: URLRequest! = try params[0].unwrap()
+            
+            requestUrl = request.url?.absoluteString
+            sentBody = request.httpBody
+            return (Data(), HTTPURLResponse())
+        }
+        
+        try await pushClient.registerPushToken(pushToken)
+        
+        XCTAssertEqual(count, 1)
+        XCTAssertEqual(requestUrl, "https://base.me-client.eservice.emarsys.net/v3/apps/EMS11-C3FD3/client/push-token")
+        XCTAssertEqual(sentBody, expectedBody)
+    }
+    
+    func testRemovePushToken_shouldRemoveTokenWithEmarsysClient() async throws {
+        var count = 0
+        var requestUrl: String? = nil
+        var requestMethod: String? = nil
+        
+        fakeNetworkClient.when(\.send) { invocationCount, params in
+            count = invocationCount
+            let request: URLRequest! = try params[0].unwrap()
+            
+            requestUrl = request.url?.absoluteString
+            requestMethod = request.httpMethod
+            return (Data(), HTTPURLResponse())
+        }
+        
+        try await pushClient.removePushToken()
+        
+        XCTAssertEqual(count, 1)
+        XCTAssertEqual(requestUrl, "https://base.me-client.eservice.emarsys.net/v3/apps/EMS11-C3FD3/client/push-token")
+        XCTAssertEqual(requestMethod, "DELETE")
+    }
 }
