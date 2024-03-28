099b2c5e059155149788eb44020a570180b9468d
David Schuppa <32750715+davidSchuppa@users.noreply.github.com>
2024-03-28 16:43:35 +0100

    chore(project): fix pbxproj by adding missing files in HostApp SUITEDEV-35392
    Co-authored-by: LasOri <24588073+LasOri@users.noreply.github.com>

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.7
+// swift-tools-version: 5.9
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -15,7 +15,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "git@github.com:emartech/mimic.git", .upToNextMinor(from: "0.1.0")),
+        .package(url: "https://github.com/emartech/mimic.git", .upToNextMinor(from: "0.1.0")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
