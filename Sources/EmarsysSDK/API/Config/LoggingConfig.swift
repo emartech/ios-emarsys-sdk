9d9bb597cbe5c9d7d45981507c8876c4cf107e0f
LordAndras <sarrotanba@gmail.com>
2024-04-05 15:13:39 +0200

    feat(config): implement logging instance SUITEDEV-32827
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>

--- Sources/EmarsysSDK/API/Config/LoggingConfig.swift
+++ Sources/EmarsysSDK/API/Config/LoggingConfig.swift
@@ -11,10 +11,14 @@ struct LoggingConfig: ConfigInstance {
     let logger: SdkLogger
     
     func changeApplicationCode(applicationCode: String) async throws {
-        
+        let entry = LogEntry.createMethodNotAllowedEntry(source: self,
+                                                         params: ["applicationCode": applicationCode])
+        logger.log(logEntry: entry, level: .debug)
     }
     
     func changeMerchantId(merchantId: String) async throws {
-        
+        let entry = LogEntry.createMethodNotAllowedEntry(source: self,
+                                                         params: ["merchantId": merchantId])
+        logger.log(logEntry: entry, level: .debug)
     }
 }
