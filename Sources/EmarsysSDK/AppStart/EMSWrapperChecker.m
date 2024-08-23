2f9e7cfdffd0ec6bb38bc69f6554372ad93709be
LasOri <LasOri@users.noreply.github.com>
2024-08-23 14:27:58 +0200

    feat(wrapper): rewise wrapperchecker SUITEDEV-36440
    Co-authored-by: davidSchuppa <32750715+davidSchuppa@users.noreply.github.com>
    Co-authored-by: megamegax <megamegax@users.noreply.github.com>

--- Sources/EmarsysSDK/AppStart/EMSWrapperChecker.m
+++ Sources/EmarsysSDK/AppStart/EMSWrapperChecker.m
@@ -4,26 +4,38 @@
 
 #import "EMSWrapperChecker.h"
 #import "EMSDispatchWaiter.h"
+#import "EMSStorageProtocol.h"
 
 @interface EMSWrapperChecker ()
 
 @property(nonatomic, strong) NSOperationQueue *queue;
 @property(nonatomic, strong) EMSDispatchWaiter *waiter;
 @property(nonatomic, strong) NSString *innerWrapper;
+@property(nonatomic, strong) id<EMSStorageProtocol>storage;
 
 @end
 
 @implementation EMSWrapperChecker
 
 - (instancetype)initWithOperationQueue:(NSOperationQueue *)queue
-                                waiter:(EMSDispatchWaiter *)waiter {
+                                waiter:(EMSDispatchWaiter *)waiter
+                               storage:(id<EMSStorageProtocol>)storage {
     NSParameterAssert(queue);
     NSParameterAssert(waiter);
+    NSParameterAssert(storage);
     if (self = [super init]) {
+        _storage = storage;
         _queue = queue;
         _waiter = waiter;
+        _innerWrapper = [self.storage stringForKey:kInnerWrapperKey];
     }
     return self;
+}
+
+- (void)setInnerWrapper:(NSString *)innerWrapper {
+    _innerWrapper = innerWrapper;
+    [self.storage setString:innerWrapper
+                     forKey:kInnerWrapperKey];
 }
 
 - (NSString *)wrapper {
@@ -31,15 +43,20 @@
         self.innerWrapper = @"none";
         [self.waiter enter];
         __weak typeof(self) weakSelf = self;
-        [[NSNotificationCenter defaultCenter] addObserverForName:@"EmarsysSDKWrapperExist"
-                                                          object:nil
-                                                           queue:self.queue
-                                                      usingBlock:^(NSNotification *note) {
-                                                          weakSelf.innerWrapper = note.object;
-                                                          [weakSelf.waiter exit];
-                                                      }];
-        [[NSNotificationCenter defaultCenter] postNotificationName:@"EmarsysSDKWrapperCheckerNotification"
-                                                            object:nil];
+        __weak __block id observer = [[NSNotificationCenter defaultCenter] addObserverForName:@"EmarsysSDKWrapperExist"
+                                                                        object:nil
+                                                                         queue:nil
+                                                                    usingBlock:^(NSNotification *note) {
+            [[NSNotificationCenter defaultCenter] removeObserver:observer];
+            [weakSelf.queue addOperationWithBlock:^{
+                weakSelf.innerWrapper = note.object;
+                [weakSelf.waiter exit];
+            }];
+        }];
+        dispatch_async(dispatch_get_main_queue(), ^{
+            [[NSNotificationCenter defaultCenter] postNotificationName:@"EmarsysSDKWrapperCheckerNotification"
+                                                                object:nil];
+        });
         [self.waiter waitWithInterval:1];
     }
     return self.innerWrapper;
